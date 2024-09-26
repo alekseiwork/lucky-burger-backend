00d9ccf83df08cb4f14d5032f2e9bdf67926c353
moisei18 <moisei118@gmail.com>
2024-09-26 19:24:54 +0200

    initial commit

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,7 @@ import { server } from './app';
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
-  console.log(`Server is on port ${PORT}`);
+  const address = server.address();
+  const host = typeof address === 'string' ? address : address?.address;
+  console.log(`Server is running at http://${host}:${PORT}`);
 });
