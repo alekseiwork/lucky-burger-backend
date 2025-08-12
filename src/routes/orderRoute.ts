32c4ebcb81911580a511874e4b85422b0ad47cb0
alekseiwork <aleksei.workmail93@gmail.com>
2025-08-13 00:15:51 +0200

    orderRoute.ts

--- src/routes/orderRoute.ts
+++ src/routes/orderRoute.ts
@@ -9,16 +9,12 @@ import { adminAuth } from '../middleware/authMiddleware';
 
 const router = Router();
 
-// Creating new order
 router.post('/', createOrder);
 
-// Getting all orders
 router.get('/', adminAuth, getOrders);
 
-// Updating order status
 router.patch('/:id', adminAuth, updateOrder);
 
-// Deleting order
 router.delete('/:id', adminAuth, deleteOrder);
 
 export default router;
