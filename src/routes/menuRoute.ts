0ec306437a88da88ecdb5c5a7f17987dfe303be2
alekseiwork <aleksei.workmail93@gmail.com>
2025-08-13 00:15:28 +0200

    menuRoute.ts

--- src/routes/menuRoute.ts
+++ src/routes/menuRoute.ts
@@ -5,13 +5,10 @@ import { createMenu, getMenu, deleteMenu } from '../controllers/menuController';
 
 const router = Router();
 
-// Creating new menu
 router.post('/', adminAuth, createMenu);
 
-// Getting the menu
 router.get('/', getMenu);
 
-// Deleting the menu
 router.delete('/:id', adminAuth, deleteMenu);
 
 // Uploading menu
